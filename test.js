7f7e3b38295a771c53c60b07c8a50e8c7e72e6cb
mereoleona22 <bala123verakishore@gmail.com>
2024-08-09 17:14:39 +0530

    creation

---
+++ test.js
@@ -0,0 +1,10 @@
+const accountID = 12345
+let accountEmail = "test@example.com"
+accountAddress = "vizag"
+let accountState;
+
+accountEmail = "kishore@example.com";
+accountAddress = "pune"
+
+console.table([accountID, accountEmail,accountAddress, accountState]);
+ 
