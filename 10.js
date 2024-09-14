387a9f93970c33cf46c83a110164470f00af1a9c
mereoleona22 <bala123verakishore@gmail.com>
2024-09-15 00:38:48 +0530

    creation

---
+++ 10.js
@@ -0,0 +1,18 @@
+//array
+
+const myArr = [1, 2, 3, 4, 5]
+const heros = ["hero1", "hero2", "hero3", "hero4"]
+console.log(myArr[0]);
+
+// array methods
+
+myArr.push(7);
+myArr.push(8);
+myArr.push(9);
+myArr.pop();
+
+myArr.unshift(1)
+
+
+console.log(myArr);
+
